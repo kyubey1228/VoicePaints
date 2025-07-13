2c5a59a4299e0310d14efc2cff17415910a24693
kyubey1228 <kyuuka1228@gmail.com>
2025-07-14 07:58:37 +0900

    公開ファイルとスタイル: アプリケーションのエントリーポイントを追加
    - public/: PWA対応のマニフェストとアイコン
    - index.tsx: Reactアプリケーションのエントリーポイント
    - App.tsx: アプリケーションのルートコンポーネント
    - index.css: Tailwind CSSとグローバルスタイル
    - App.css: アプリケーション固有のスタイル

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VoicePaintApp from './components/VoicePaintApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <VoicePaintApp />
+    </div>
+  );
+}
+
+export default App;
